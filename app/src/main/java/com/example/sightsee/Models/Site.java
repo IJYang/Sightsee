1de2e4ebbc6821e2dbd24aad9bf9db91d3a106a9
inseo <inseob.yang1@gmail.com>
2021-02-11 17:11:50 -0800

    Added test images for sites and created a static list of sites for testing.

--- app/src/main/java/com/example/sightsee/Models/Site.java
+++ app/src/main/java/com/example/sightsee/Models/Site.java
@@ -1,5 +1,7 @@
 package com.example.sightsee.Models;
 
+import com.example.sightsee.R;
+
 public class Site {
     private String name;
     private String type;
@@ -24,4 +26,12 @@ public class Site {
         this.imageResourceId = imgResource;
         this.address = address;
     }
+
+    public static final Site[] test_sites = {
+            new Site("Science World", "Museum", R.drawable.science_world, "1455 Quebec St, Vancouver BC"),
+            new Site("Vancouver Aquarium", "Museum", R.drawable.vancouver_aquarium, "845 Avison Way, Vancouver BC"),
+            new Site("Capilano Suspension Park", "Hiking", R.drawable.capilano, "3735 Capilano Rd, North Vancouver BC"),
+            new Site("Stanley Park", "Trails", R.drawable.seawall, "Vancouver BC, V6G 1Z4"),
+            new Site("Sea to Sky", "Hiking", R.drawable.sea_to_sky, "36800 BC-99, Squamish BC")
+    };
 }
