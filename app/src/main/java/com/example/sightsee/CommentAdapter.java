f8a14b7864ebb0a031c8d2c25bcba5f72a7dde30
IJYang <inseob.yang1@gmail.com>
2021-04-11 11:53:39 -0700

    Cleaned comment adapter, comment fragment, and comment activity

--- app/src/main/java/com/example/sightsee/CommentAdapter.java
+++ app/src/main/java/com/example/sightsee/CommentAdapter.java
@@ -7,13 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-
 import com.example.sightsee.Models.Comment;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 public class CommentAdapter extends ArrayAdapter<Comment> {
